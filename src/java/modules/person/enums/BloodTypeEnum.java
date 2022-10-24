9b26b06ce14c3260939b5d969559fe35b5c113f4
https://gitlab.com/FlavioInacio-jf <jflavioinacio22@gmail.com>
2022-10-24 09:43:40 -0300

    refactor: Criado o model relacionado a pessoa

---
+++ src/java/modules/person/enums/BloodTypeEnum.java
@@ -0,0 +1,5 @@
+package java.modules.person.enums;
+
+public enum BloodTypeEnum {
+    A, A_PLUS, A_LESS;
+}
--- src/java/modules/person/models/Person.java
+++ src/java/modules/person/models/Person.java
@@ -1,5 +1,64 @@
 package java.modules.person.models;
 
+import java.modules.person.enums.BloodTypeEnum;
+import java.util.UUID;
+
 public class Person {
-    private String name;
+    private String _id;
+    private String firstName;
+
+    private String lastName;
+
+    private String cpf;
+    private BloodTypeEnum bloodType;
+    private byte age;
+
+    public Person(
+            String _id,
+            String firstName,
+            String lastName,
+            String cpf,
+            BloodTypeEnum bloodType,
+            byte age
+    ) {
+        this._id = _id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.cpf = cpf;
+        this.bloodType = bloodType;
+        this.age =  age;
+
+        if (this._id == null) {
+            this._id = UUID.randomUUID().toString();
+        }
+    }
+
+    public String getId(){
+
+        return this._id;
+    }
+    public String getFirstName() {
+        return this.firstName;
+    }
+
+    public String getLastName() {
+        return this.lastName;
+    }
+
+    public String getCpt() {
+        return this.cpf;
+    }
+
+    public BloodTypeEnum bloodType() {
+        return this.bloodType;
+    }
+
+    public byte getAge() {
+        return this.age;
+    }
+
+    public String getFullName() {
+        return this.firstName + this.lastName;
+    }
+
 }
