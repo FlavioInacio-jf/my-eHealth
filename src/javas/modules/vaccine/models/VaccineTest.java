3c65a357bd933cdd5f86157b45137fa8f37c40a6
https://gitlab.com/FlavioInacio-jf <jflavioinacio22@gmail.com>
2022-11-09 17:55:56 -0300

    refactor: Definido a classe de teste do model Vaccine

---
+++ src/javas/modules/vaccine/models/VaccineTest.java
@@ -0,0 +1,58 @@
+package javas.modules.vaccine.models;
+
+import javas.modules.heathUnit.models.HeathUnit;
+import org.junit.Test;
+
+public class VaccineTest {
+    public VaccineTest() {}
+
+    @Test
+    public void getName() {
+
+    }
+
+    @Test
+    public void setName(String Name) {
+
+    }
+
+    @Test
+    public void getDate() {
+
+    }
+
+    @Test
+    public void setDate(String date) {
+
+    }
+
+    @Test
+    public void getDose() {
+
+    }
+
+    @Test
+    public void setDose() {
+
+    }
+
+    @Test
+    public void getLot() {
+
+    }
+
+    @Test
+    public void setLot() {
+
+    }
+
+    @Test
+    public void getHeathUnit() {
+
+    }
+
+    @Test
+    public void setHeathUnit() {
+
+    }
+}
