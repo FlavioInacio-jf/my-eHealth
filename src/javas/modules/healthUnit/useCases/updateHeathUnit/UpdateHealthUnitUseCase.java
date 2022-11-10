6d0578b3f04451465b77b09b1a1af10fc634b932
https://gitlab.com/FlavioInacio-jf <jflavioinacio22@gmail.com>
2022-11-10 16:49:06 -0300

    refactor: add caso de uso de atualização da unidade de saúde

--- src/javas/modules/healthUnit/useCases/updateHeathUnit/UpdateHealthUnitUseCase.java
+++ src/javas/modules/healthUnit/useCases/updateHeathUnit/UpdateHealthUnitUseCase.java
@@ -1,5 +1,8 @@
 package javas.modules.healthUnit.useCases.updateHeathUnit;
 
+import javas.errors.CustomError;
+import javas.exceptions.HealthUnitErrorMessages;
+import javas.modules.healthUnit.models.HealthUnit;
 import javas.modules.healthUnit.repositories.IHealthUnitRepository;
 
 public class UpdateHealthUnitUseCase {
@@ -9,7 +12,15 @@ public class UpdateHealthUnitUseCase {
         this.heathUnitRepository = heathUnitRepository;
     }
 
-    public void handle() {
-
+    public void handle(HealthUnit healthUnit) {
+        HealthUnit healthUnitExists =  this.heathUnitRepository.findByCNPJ(healthUnit.getCNPJ());
+        if (healthUnitExists == null) {
+            throw new CustomError(HealthUnitErrorMessages.HEALTH_UNIT_NOT_FOUND_TITLE,
+                    HealthUnitErrorMessages.HEALTH_UNIT_NOT_FOUND_DETAIL);
+        }
+        healthUnitExists.setId(healthUnitExists.getId());
+        healthUnitExists.setName(healthUnit.getName());
+        healthUnitExists.setAddress(healthUnit.getAddress());
+        this.heathUnitRepository.update(healthUnitExists);
     }
 }
