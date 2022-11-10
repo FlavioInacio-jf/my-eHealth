fddee3a263781fd9937145ec6c3635887b65cd01
https://gitlab.com/FlavioInacio-jf <jflavioinacio22@gmail.com>
2022-11-10 09:16:31 -0300

    refactor: Iniciado a adição do repositorio de vacina

--- src/javas/constants/VaccineEntityConstants.java
+++ src/javas/constants/VaccineEntityConstants.java
@@ -16,7 +16,7 @@ public class VaccineEntityConstants {
                 String.format("( %s TEXT NOT NULL,", ID_COLUMN_NAME) +
                 String.format("%s TEXT NOT NULL,", NAME_COLUMN_NAME) +
                 String.format("%s TEXT NOT NULL,", DATE_COLUMN_NAME) +
-                String.format("%s TEXT NOT NULL UNIQUE,", DOSE_COLUMN_NAME) +
+                String.format("%s INT NOT NULL,", DOSE_COLUMN_NAME) +
                 String.format("%s TEXT NOT NULL,", LOT_COLUMN_NAME) +
                 String.format("%s TEXT NOT NULL,", HEATH_UNIT_COLUMN_NAME_FK) +
                 String.format("%s TEXT NOT NULL", PERSON_COLUMN_NAME_FK) +
--- src/javas/exceptions/VaccineErrorMessages.java
+++ src/javas/exceptions/VaccineErrorMessages.java
@@ -1,4 +1,10 @@
 package javas.exceptions;
 
 public class VaccineErrorMessages {
+    public static final String VACCINE_NOT_FOUND_TITLE = "Vacina não foi encontrado";
+    public static final String UNABLE_UPDATE_VACCINE = "Não foi possível atualizar a vacina!";
+    public static final String UNABLE_CREATE_VACCINE= "Não foi possível criar a vacina!";
+    public static final String UNABLE_DELETE_VACCINE =  "Não foi possível excluir a vacina!";
+    public static final String UNABLE_SEARCH_VACCINE =  "Não foi possível buscar a vacina!";
+    public static final String FAILED_CONVERT_RESULT_SET_TO_VACCINE = "Falha ao converter ResultSet para Vaccine";
 }
