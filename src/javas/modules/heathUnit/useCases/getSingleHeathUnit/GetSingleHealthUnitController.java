dd18908e1c6368733ba4a98794de6b8e47d0f3dd
https://gitlab.com/FlavioInacio-jf <jflavioinacio22@gmail.com>
2022-11-10 16:35:51 -0300

    refactor: Add caso de uso em healthUnit

--- src/javas/modules/heathUnit/useCases/getSingleHeathUnit/GetSingleHealthUnitController.java
+++ src/javas/modules/heathUnit/useCases/getSingleHeathUnit/GetSingleHealthUnitController.java
@@ -1,4 +1,16 @@
 package javas.modules.heathUnit.useCases.getSingleHeathUnit;
 
 public class GetSingleHealthUnitController {
+    GetSingleHealthUnitUseCase getSingleHealthUnitUseCase;
+
+    public GetSingleHealthUnitController(GetSingleHealthUnitUseCase getSingleHealthUnitUseCase){
+        this.getSingleHealthUnitUseCase = getSingleHealthUnitUseCase;
+    }
+    public void execute (String cnpj){
+        try {
+            this.getSingleHealthUnitUseCase.handle(cnpj);
+        }catch (Error error) {
+            throw new Error(error);
+        }
+    }
 }
--- src/javas/modules/heathUnit/useCases/getSingleHeathUnit/GetSingleHealthUnitUseCase.java
+++ src/javas/modules/heathUnit/useCases/getSingleHeathUnit/GetSingleHealthUnitUseCase.java
@@ -1,5 +1,8 @@
 package javas.modules.heathUnit.useCases.getSingleHeathUnit;
 
+import javas.errors.CustomError;
+import javas.exceptions.HealthUnitErrorMessages;
+import javas.modules.heathUnit.models.HealthUnit;
 import javas.modules.heathUnit.repositories.IHealthUnitRepository;
 
 public class GetSingleHealthUnitUseCase {
@@ -9,7 +12,13 @@ public class GetSingleHealthUnitUseCase {
         this.heathUnitRepository = heathUnitRepository;
     }
 
-    public void handle() {
+    public HealthUnit handle(String cnpj) {
+        HealthUnit healthUnitExists =  this.heathUnitRepository.findByCNPJ(cnpj);
+        if (healthUnitExists == null) {
+            throw new CustomError(HealthUnitErrorMessages.HEALTH_UNIT_NOT_FOUND_TITLE,
+                    HealthUnitErrorMessages.HEALTH_UNIT_NOT_FOUND_DETAIL);
+        }
 
+       return healthUnitExists;
     }
 }
