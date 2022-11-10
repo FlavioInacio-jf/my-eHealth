526eac4fe48200d47af02b7e9eaa0cf50caa5e8b
https://gitlab.com/FlavioInacio-jf <jflavioinacio22@gmail.com>
2022-11-10 15:39:36 -0300

    refactor: Add casos de uso do model de paciente

--- src/javas/modules/person/useCases/getAllPeople/GetAllPeopleUseCase.java
+++ src/javas/modules/person/useCases/getAllPeople/GetAllPeopleUseCase.java
@@ -3,13 +3,15 @@ package javas.modules.person.useCases.getAllPeople;
 import javas.modules.person.models.Person;
 import javas.modules.person.repositories.IPersonRepository;
 
+import java.util.ArrayList;
+
 public class GetAllPeopleUseCase {
     private IPersonRepository personRepository;
     public GetAllPeopleUseCase(IPersonRepository personRepository) {
         this.personRepository = personRepository;
     }
 
-    public Person[] handle() {
-        return null;
+    public ArrayList<Person> handle() {
+        return this.personRepository.getAll();
     }
 }
--- src/javas/modules/person/useCases/getSinglePerson/GetSinglePersonController.java
+++ src/javas/modules/person/useCases/getSinglePerson/GetSinglePersonController.java
@@ -1,12 +1,17 @@
 package javas.modules.person.useCases.getSinglePerson;
 
+import javas.modules.person.models.Person;
+
 public class GetSinglePersonController {
     private GetSinglePersonUseCase getSinglePersonUseCase;
     public GetSinglePersonController(GetSinglePersonUseCase getSinglePersonUseCase) {
         this.getSinglePersonUseCase = getSinglePersonUseCase;
     }
-
-    public void execute() {
-        this.getSinglePersonUseCase.handle();
+    public Person execute(String cpf) {
+        try {
+            return this.getSinglePersonUseCase.handle(cpf);
+        }catch (Error error) {
+            throw new Error(error);
+        }
     }
 }
