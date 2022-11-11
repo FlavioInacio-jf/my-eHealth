e44e6b2d7b00229b05663bc326af73845b7c7fee
Rhessaw0 <chryriathemaster@gmail.com>
2022-11-11 07:23:52 -0300

    apply changes to some test methods in VaccineTest.java

--- src/javas/modules/vaccine/models/VaccineTest.java
+++ src/javas/modules/vaccine/models/VaccineTest.java
@@ -7,42 +7,58 @@ public class VaccineTest {
 
     @Test
     public void getName() {
-
+        Vaccine vaccine = new Vaccine(null, "Hepatite C", "14/02/2020", 1, "157");
+        assertEquals("Hepatite C", vaccine.getName());
     }
 
     @Test
     public void setName(String Name) {
-
+        Vaccine vaccine = new Vaccine(null, "Hepatite C", "14/02/2020", 1, "157");
+        assertEquals("Hepatite C", vaccine.getName());
+        vaccine.setName("Influenza");
+        assertEquals("Influenza", vaccine.getName());
     }
 
     @Test
     public void getDate() {
-
+        Vaccine vaccine = new Vaccine(null, "Hepatite C", "14/02/2020", 1, "157");
+        assertEquals("14/02/2020", vaccine.getDate());
     }
 
     @Test
     public void setDate(String date) {
-
+        Vaccine vaccine = new Vaccine(null, "Hepatite C", "14/02/2020", 1, "157");
+        assertEquals("14/02/2020", vaccine.getDate());
+        vaccine.setDate("01/06/2021");
+        assertEquals("01/06/2021", vaccine.getDate());
     }
 
     @Test
     public void getDose() {
-
+        Vaccine vaccine = new Vaccine(null, "Hepatite C", "14/02/2020", 1, "157");
+        assertEquals(1, vaccine.getDose());
     }
 
     @Test
     public void setDose() {
-
+        Vaccine vaccine = new Vaccine(null, "Hepatite C", "14/02/2020", 1, "157");
+        assertEquals(1, vaccine.getDose());
+        vaccine.setDose(4);
+        assertEquals(4, vaccine.getDose());
     }
 
     @Test
     public void getLot() {
-
+        Vaccine vaccine = new Vaccine(null, "Hepatite C", "14/02/2020", 1, "157");
+        assertEquals("157", vaccine.getLot());
     }
 
     @Test
     public void setLot() {
-
+        Vaccine vaccine = new Vaccine(null, "Hepatite C", "14/02/2020", 1, "157");
+        assertEquals("157", vaccine.getLot());
+        vaccine.setLot("2361");
+        assertEquals("2361", vaccine.getLot());
     }
 
     @Test
