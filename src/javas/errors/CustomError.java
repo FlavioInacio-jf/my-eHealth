1ad486db38723db89746560aa2b506e6c62725b6
https://gitlab.com/FlavioInacio-jf <jflavioinacio22@gmail.com>
2022-11-11 23:34:48 -0300

    refactor: Corrigido formatação das mensagens de erros

--- src/javas/errors/CustomError.java
+++ src/javas/errors/CustomError.java
@@ -2,6 +2,6 @@ package javas.errors;
 
 public class CustomError extends Error {
     public CustomError(String title, String detail) {
-        super(title + ": " + detail);
+        super(title + detail);
     }
 }
