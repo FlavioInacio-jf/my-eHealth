50c72e6e767f7dca1fc40bcb29f5856af4e656c4
Rhessaw0 <chryriathemaster@gmail.com>
2022-11-14 10:58:24 -0300

    apply changes to some test methods in PersonRepositoryTest.java

--- src/javas/modules/vaccine/repositories/impementations/VaccineRepositoryTest.java
+++ src/javas/modules/vaccine/repositories/impementations/VaccineRepositoryTest.java
@@ -1,12 +1,24 @@
 package javas.modules.vaccine.repositories.impementations;
 
+import javas.modules.person.models.Vaccine;
+import javas.modules.person.repositories.IVaccineRepository;
 import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class VaccineRepositoryTest {
 
+    public IVaccineRepository repository;
+
+    public VaccineRepositoryTest() {this.repository = new VaccineRepository();}
 
     @Test
     public void testCreate() {
+        Vaccine vaccine = new Vaccine(null, "Hepatite C", 1, "157");
+        this.repository.crate(null, null, vaccine);
 
     }
 
