55b1188d453fcad29a74f43ca47b4ccb65573c2b
https://gitlab.com/FlavioInacio-jf <jflavioinacio22@gmail.com>
2022-11-15 18:41:55 -0300

    refactor: add getSingleHealthUnit tests

--- src/tests/javas/GetSingleHealthUnitControllerTest.java
+++ src/tests/javas/GetSingleHealthUnitControllerTest.java
@@ -1,10 +1,41 @@
 package tests.javas;
 
+import javas.modules.app.models.Address;
+import javas.modules.healthUnit.enums.UnitTypeEnum;
+import javas.modules.healthUnit.models.HealthUnit;
+import javas.modules.healthUnit.repositories.IHealthUnitRepository;
+import javas.modules.healthUnit.repositories.implementations.HealthUnitRepository;
+import javas.modules.healthUnit.useCases.getSingleHealthUnit.GetSingleHealthUnitController;
+import javas.modules.healthUnit.useCases.getSingleHealthUnit.GetSingleHealthUnitUseCase;
 import org.junit.Test;
 
+import static org.junit.Assert.*;
+
 public class GetSingleHealthUnitControllerTest {
-    public GetSingleHealthUnitControllerTest() {}
+    IHealthUnitRepository healthUnitRepository;
+    GetSingleHealthUnitUseCase getSingleHealthUnitUseCase;
+    public GetSingleHealthUnitControllerTest() {
+        this.healthUnitRepository = new HealthUnitRepository();
+        this.getSingleHealthUnitUseCase = new GetSingleHealthUnitUseCase(this.healthUnitRepository);
+    }
 
     @Test
-    public void execute() {}
+    public void execute() {
+        GetSingleHealthUnitController getSingleHealthUnitController = new GetSingleHealthUnitController(getSingleHealthUnitUseCase);
+
+        Address address = new Address("Rua flavao", "Bairro Oliveira",
+                "Aracaju", "Sergipe", "987909123");
+        HealthUnit healthUnit = new HealthUnit(null, UnitTypeEnum.HOSPITAL,
+                "Hospital Inacio", "13.659.127/0001-24", address);
+
+        this.healthUnitRepository.create(healthUnit);
+        assertTrue(getSingleHealthUnitController.execute(healthUnit.getCNPJ()).equals(healthUnit));
+        this.healthUnitRepository.delete(healthUnit.getId());
+
+        try {
+            assertNull(getSingleHealthUnitController.execute(healthUnit.getCNPJ()));
+            fail();
+        }catch (Error ignored) {
+        }
+    }
 }
--- src/tests/javas/GetSingleHealthUnitUseCaseTest.java
+++ src/tests/javas/GetSingleHealthUnitUseCaseTest.java
@@ -1,10 +1,39 @@
 package tests.javas;
 
+import javas.modules.app.models.Address;
+import javas.modules.healthUnit.enums.UnitTypeEnum;
+import javas.modules.healthUnit.models.HealthUnit;
+import javas.modules.healthUnit.repositories.IHealthUnitRepository;
+import javas.modules.healthUnit.repositories.implementations.HealthUnitRepository;
+import javas.modules.healthUnit.useCases.getSingleHealthUnit.GetSingleHealthUnitUseCase;
 import org.junit.Test;
 
+import static org.junit.Assert.*;
+
+
 public class GetSingleHealthUnitUseCaseTest {
-    public GetSingleHealthUnitUseCaseTest() {}
+    IHealthUnitRepository healthUnitRepository;
+    public GetSingleHealthUnitUseCaseTest() {
+        this.healthUnitRepository = new HealthUnitRepository();
+    }
 
     @Test
-    public void testHandle() {}
+    public void testHandle() {
+        GetSingleHealthUnitUseCase getSingleHealthUnitUseCase  = new GetSingleHealthUnitUseCase(this.healthUnitRepository);
+
+        Address address = new Address("Rua flavao", "Bairro Oliveira",
+                "Aracaju", "Sergipe", "987909123");
+        HealthUnit healthUnit = new HealthUnit(null, UnitTypeEnum.HOSPITAL,
+                "Hospital Inacio", "13.789.127/0001-24", address);
+
+        this.healthUnitRepository.create(healthUnit);
+        assertTrue(getSingleHealthUnitUseCase.handle(healthUnit.getCNPJ()).equals(healthUnit));
+        this.healthUnitRepository.delete(healthUnit.getCNPJ());
+
+        try {
+            assertNull(getSingleHealthUnitUseCase.handle(healthUnit.getCNPJ()));
+            fail();
+        }catch (Error ignored) {
+        }
+    }
 }
