af26445aaef1060001c41c3a8bb68410839a8c8f
https://gitlab.com/FlavioInacio-jf <jflavioinacio22@gmail.com>
2022-11-15 18:09:19 -0300

    refactor: Add CreateHealthUnit Tests

--- src/tests/javas/CreateHealthUnitControllerTest.java
+++ src/tests/javas/CreateHealthUnitControllerTest.java
@@ -1,11 +1,40 @@
 package tests.javas;
 
+import javas.modules.healthUnit.repositories.IHealthUnitRepository;
+import javas.modules.healthUnit.repositories.implementations.HealthUnitRepository;
+import javas.modules.healthUnit.useCases.createHealthUnit.CreateHealthUnitController;
+import javas.modules.healthUnit.useCases.createHealthUnit.CreateHealthUnitUseCase;
 import org.junit.Test;
 
+import static org.junit.Assert.fail;
+
 public class CreateHealthUnitControllerTest {
-    public CreateHealthUnitControllerTest() {}
+    IHealthUnitRepository healthUnitRepository;
+    CreateHealthUnitUseCase createHealthUnitUseCase;
+    public CreateHealthUnitControllerTest() {
+        this.healthUnitRepository = new HealthUnitRepository();
+        this.createHealthUnitUseCase = new CreateHealthUnitUseCase(this.healthUnitRepository);
+    }
 
     @Test
-    public void execute() {}
+    public void execute() {
+        CreateHealthUnitController createHealthUnitController = new CreateHealthUnitController(this.createHealthUnitUseCase);
+        try {
+            createHealthUnitController.execute("UBS", "Hospital Inacio", "21.987.127/0001-24",
+                    "Rua flavao", "Bairro Oliveira",
+                    "Aracaju", "Sergipe", "44445-000");
+        } catch (IllegalAccessException | Error e) {
+            fail();
+        }
+
+        try {
+            createHealthUnitController.execute("UBS", "Hospital Inacio", "",
+                    "Rua flavao", "Bairro Oliveira",
+                    "Aracaju", "Sergipe", "44445-000");
+            fail();
+        } catch (IllegalAccessException | Error e) {
+        }
+        this.healthUnitRepository.delete(this.healthUnitRepository.findByCNPJ("21.987.127/0001-24").getId());
+    }
 
 }
--- src/tests/javas/CreateHealthUnitUseCaseTest.java
+++ src/tests/javas/CreateHealthUnitUseCaseTest.java
@@ -1,10 +1,39 @@
 package tests.javas;
 
+import javas.modules.app.models.Address;
+import javas.modules.healthUnit.enums.UnitTypeEnum;
+import javas.modules.healthUnit.models.HealthUnit;
+import javas.modules.healthUnit.repositories.IHealthUnitRepository;
+import javas.modules.healthUnit.repositories.implementations.HealthUnitRepository;
+import javas.modules.healthUnit.useCases.createHealthUnit.CreateHealthUnitUseCase;
 import org.junit.Test;
 
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
 public class CreateHealthUnitUseCaseTest {
-    public CreateHealthUnitUseCaseTest() {}
+    IHealthUnitRepository healthUnitRepository;
+    public CreateHealthUnitUseCaseTest() {
+        this.healthUnitRepository = new HealthUnitRepository();
+    }
 
     @Test
-    public void testHandle() {}
+    public void testHandle() {
+        CreateHealthUnitUseCase createHealthUnitUseCase = new CreateHealthUnitUseCase(this.healthUnitRepository);
+        Address address = new Address("Rua flavao", "Bairro Oliveira",
+                "Aracaju", "Sergipe", "987909123");
+
+        HealthUnit healthUnit = new HealthUnit(null, UnitTypeEnum.HOSPITAL,
+                "Hospital Inacio", "21.456.127/0001-24", address);
+
+        createHealthUnitUseCase.handle(healthUnit);
+        assertTrue(this.healthUnitRepository.findById(healthUnit.getId()).equals(healthUnit));
+        this.healthUnitRepository.delete(healthUnit.getId());
+        try {
+            createHealthUnitUseCase.handle(healthUnit);
+            fail();
+        }catch (Error ignored) {
+
+        }
+    }
 }
