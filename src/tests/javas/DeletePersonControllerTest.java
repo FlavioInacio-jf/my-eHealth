3e6a2fbda5147e117d6a550a04b925e9b86669d8
Arthur6742 <113526653+Arthur6742@users.noreply.github.com>
2022-11-15 19:46:07 -0300

    DeletePersonController
    Refector adicionou-se o deletePersonControllerTest

--- src/tests/javas/DeletePersonControllerTest.java
+++ src/tests/javas/DeletePersonControllerTest.java
@@ -1,12 +1,40 @@
 package tests.javas;
 
+
+import javas.modules.person.enums.BloodTypeEnum;
+import javas.modules.person.enums.SexEnum;
+import javas.modules.person.models.Person;
+import javas.modules.person.repositories.IPersonRepository;
+import javas.modules.person.repositories.implementations.PersonRepository;
+import javas.modules.person.useCases.deletePerson.DeletePersonController;
+import javas.modules.person.useCases.deletePerson.DeletePersonUseCase;
 import org.junit.Test;
 
+import static org.junit.Assert.*;
+
 public class DeletePersonControllerTest {
-    public DeletePersonControllerTest() {}
+    IPersonRepository personRepository;
+    DeletePersonUseCase deletePersonUseCase;
+    public DeletePersonControllerTest() {
+        this.personRepository = new PersonRepository();
+        this.deletePersonUseCase = new DeletePersonUseCase(this.personRepository);
+    }
 
     @Test
     public void testExecute() {
+        DeletePersonController deletePersonController = new DeletePersonController(this.deletePersonUseCase);
+        Person person = new  Person(null, "Inácio",
+        "Santos", "000.000.000-00", BloodTypeEnum.A_LESS, SexEnum.MALE, "11/09/2001", null);
+        
+        this.personRepository.create(person);
+        assertTrue(person.equals(this.personRepository.findById(person.getId())));
+        deletePersonController.execute(person.getCPF());
+        assertNull(this.personRepository.findById(person.getId()));
 
+        try {
+            deletePersonController.execute(person.getCPF());
+            fail();
+        }catch (Error ignored) {
+        }
     }
 }
