de20cd008b99daee4064d86ee01855bdf33d0aba
Arthur6742 <113526653+Arthur6742@users.noreply.github.com>
2022-11-15 16:51:50 -0300

    createPersonController
    refector criou-se o teste de createPersonController

--- src/tests/javas/CreatePersonControllerTest.java
+++ src/tests/javas/CreatePersonControllerTest.java
@@ -1,12 +1,28 @@
 package tests.javas;
 
+import javas.modules.app.models.Address;
+import javas.modules.person.enums.BloodTypeEnum;
+import javas.modules.person.enums.SexEnum;
+import javas.modules.person.models.Person;
+
+
+import javas.validations.*;
+
 import org.junit.Test;
 
 public class CreatePersonControllerTest {
     public CreatePersonControllerTest() {}
 
     @Test
-    public void testExecute() {
-
+    public void testExecute() throws IllegalAccessException {
+        Address address = new Address("Rua teste", "bairro teste", "cidade teste", 
+        "estado teste", "13165-000");
+        
+        Person person = new Person(null, "josuel", 
+        "sobrenomeTeste", "999.888.777-23", 
+        BloodTypeEnum.A, SexEnum.MALE, "11/09/2001", address);
+        
+        new Validation(person).validate();
+        new Validation(address).validate();
     }
 }
