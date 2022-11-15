ee450d85bb2d94c4070161dde8e9df5c09fffe96
https://gitlab.com/FlavioInacio-jf <jflavioinacio22@gmail.com>
2022-11-15 19:11:47 -0300

    refactor: Add tests in Update HealthUnit

--- src/tests/javas/UpdateHealthUnitControllerTest.java
+++ src/tests/javas/UpdateHealthUnitControllerTest.java
@@ -1,10 +1,55 @@
 package tests.javas;
 
+import javas.modules.app.models.Address;
+import javas.modules.healthUnit.enums.UnitTypeEnum;
+import javas.modules.healthUnit.models.HealthUnit;
+import javas.modules.healthUnit.repositories.IHealthUnitRepository;
+import javas.modules.healthUnit.repositories.implementations.HealthUnitRepository;
+import javas.modules.healthUnit.useCases.updateHeathUnit.UpdateHealthUnitController;
+import javas.modules.healthUnit.useCases.updateHeathUnit.UpdateHealthUnitUseCase;
 import org.junit.Test;
 
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
 public class UpdateHealthUnitControllerTest {
-    public UpdateHealthUnitControllerTest() {}
+    IHealthUnitRepository healthUnitRepository;
+    UpdateHealthUnitUseCase updateHealthUnitUseCase;
+
+    public UpdateHealthUnitControllerTest() {
+        this.healthUnitRepository = new HealthUnitRepository();
+        this.updateHealthUnitUseCase = new UpdateHealthUnitUseCase(this.healthUnitRepository);
+    }
 
     @Test
-    public void execute() {}
+    public void execute() {
+        UpdateHealthUnitController updateHealthUnitController = new UpdateHealthUnitController(updateHealthUnitUseCase);
+        Address address = new Address("Rua flavao", "Bairro Oliveira",
+                "Aracaju", "Sergipe", "44444-000");
+
+        HealthUnit healthUnit = new HealthUnit(null, UnitTypeEnum.HOSPITAL,
+                "Hospital Inacio", "19.865.563/0001-24", address);
+
+        this.healthUnitRepository.create(healthUnit);
+        try {
+            this.healthUnitRepository.findById(healthUnit.getId());
+        }catch (Error ignored) {
+            fail();
+        }
+        healthUnit.setName("João Alves Filho");
+        try {
+            updateHealthUnitController.execute("HOSPITAL",
+                                                healthUnit.getName(),
+                                                healthUnit.getCNPJ(),
+                                                healthUnit.getAddress().getStreet(),
+                                                healthUnit.getAddress().getDistrict(),
+                                                healthUnit.getAddress().getCity(),
+                                                healthUnit.getAddress().getState(),
+                                                healthUnit.getAddress().getPostalCode());
+        }catch (Error | IllegalAccessException ignored) {
+            fail();
+        }
+        assertTrue(this.healthUnitRepository.findByCNPJ(healthUnit.getCNPJ()).equals(healthUnit));
+        this.healthUnitRepository.delete(healthUnit.getId());
+    }
 }
--- src/tests/javas/UpdateHealthUnitUseCaseTest.java
+++ src/tests/javas/UpdateHealthUnitUseCaseTest.java
@@ -1,10 +1,42 @@
 package tests.javas;
 
+import javas.modules.app.models.Address;
+import javas.modules.healthUnit.enums.UnitTypeEnum;
+import javas.modules.healthUnit.models.HealthUnit;
+import javas.modules.healthUnit.repositories.IHealthUnitRepository;
+import javas.modules.healthUnit.repositories.implementations.HealthUnitRepository;
+import javas.modules.healthUnit.useCases.updateHeathUnit.UpdateHealthUnitUseCase;
 import org.junit.Test;
 
+
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
 public class UpdateHealthUnitUseCaseTest {
-    public UpdateHealthUnitUseCaseTest() {}
+    IHealthUnitRepository healthUnitRepository;
+    public UpdateHealthUnitUseCaseTest() {
+        this.healthUnitRepository = new HealthUnitRepository();
+    }
 
     @Test
-    public void testHandle() {}
+    public void testHandle() {
+        UpdateHealthUnitUseCase updateHealthUnitUseCase = new UpdateHealthUnitUseCase(this.healthUnitRepository);
+        Address address = new Address("Rua flavao", "Bairro Oliveira",
+                "Aracaju", "Sergipe", "987909123");
+
+        HealthUnit healthUnit = new HealthUnit(null, UnitTypeEnum.HOSPITAL,
+                "Hospital Inacio", "19.865.563/0001-24", address);
+
+        this.healthUnitRepository.create(healthUnit);
+        try {
+            this.healthUnitRepository.findById(healthUnit.getId());
+        }catch (Error ignored) {
+            fail();
+        }
+        healthUnit.setName("João Alves Filho");
+        updateHealthUnitUseCase.handle(healthUnit);
+        assertTrue(this.healthUnitRepository.findByCNPJ(healthUnit.getCNPJ()).equals(healthUnit));
+
+        this.healthUnitRepository.delete(healthUnit.getId());
+    }
 }
