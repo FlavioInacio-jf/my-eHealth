e7be83956ca5b63fbe44845790e23ed7b086d31f
https://gitlab.com/FlavioInacio-jf <jflavioinacio22@gmail.com>
2022-11-15 18:26:16 -0300

    refactor: Add DeleteHealthUnit test

--- src/tests/javas/DeleteHealthUnitControllerTest.java
+++ src/tests/javas/DeleteHealthUnitControllerTest.java
@@ -1,10 +1,41 @@
 package tests.javas;
 
+import javas.modules.app.models.Address;
+import javas.modules.healthUnit.enums.UnitTypeEnum;
+import javas.modules.healthUnit.models.HealthUnit;
+import javas.modules.healthUnit.repositories.IHealthUnitRepository;
+import javas.modules.healthUnit.repositories.implementations.HealthUnitRepository;
+import javas.modules.healthUnit.useCases.deleteHealthUnit.DeleteHealthUnitController;
+import javas.modules.healthUnit.useCases.deleteHealthUnit.DeleteHealthUnitUseCase;
 import org.junit.Test;
 
+import static org.junit.Assert.*;
+
 public class DeleteHealthUnitControllerTest {
-    public DeleteHealthUnitControllerTest() {}
+    IHealthUnitRepository healthUnitRepository;
+    DeleteHealthUnitUseCase deleteHealthUnitUseCase;
+    public DeleteHealthUnitControllerTest() {
+        this.healthUnitRepository = new HealthUnitRepository();
+        this.deleteHealthUnitUseCase = new DeleteHealthUnitUseCase(this.healthUnitRepository);
+    }
 
     @Test
-    public void execute() {}
+    public void execute() {
+        DeleteHealthUnitController deleteHealthUnitController = new DeleteHealthUnitController(this.deleteHealthUnitUseCase);
+        Address address = new Address("Rua flavao", "Bairro Oliveira", "Aracaju", "Sergipe", "987909123");
+
+        HealthUnit healthUnit = new HealthUnit(null, UnitTypeEnum.HOSPITAL,
+                "Hospital Inacio", "21.222.333/0001-24", address);
+
+        this.healthUnitRepository.create(healthUnit);
+        assertTrue(healthUnit.equals(this.healthUnitRepository.findById(healthUnit.getId())));
+        deleteHealthUnitController.execute(healthUnit.getCNPJ());
+        assertNull(this.healthUnitRepository.findById(healthUnit.getId()));
+
+        try {
+            deleteHealthUnitController.execute(healthUnit.getCNPJ());
+            fail();
+        }catch (Error ignored) {
+        }
+    }
 }
--- src/tests/javas/DeleteHealthUnitUseCaseTest.java
+++ src/tests/javas/DeleteHealthUnitUseCaseTest.java
@@ -1,10 +1,38 @@
 package tests.javas;
 
+import javas.modules.app.models.Address;
+import javas.modules.healthUnit.enums.UnitTypeEnum;
+import javas.modules.healthUnit.models.HealthUnit;
+import javas.modules.healthUnit.repositories.IHealthUnitRepository;
+import javas.modules.healthUnit.repositories.implementations.HealthUnitRepository;
+import javas.modules.healthUnit.useCases.deleteHealthUnit.DeleteHealthUnitUseCase;
 import org.junit.Test;
 
+import static org.junit.Assert.*;
+
 public class DeleteHealthUnitUseCaseTest {
-    public DeleteHealthUnitUseCaseTest() {}
+    IHealthUnitRepository healthUnitRepository;
+    public DeleteHealthUnitUseCaseTest() {
+        this.healthUnitRepository = new HealthUnitRepository();
+    }
 
     @Test
-    public void testHandle() {}
+    public void testHandle() {
+        DeleteHealthUnitUseCase deleteHealthUnitUseCase = new DeleteHealthUnitUseCase(this.healthUnitRepository);
+        Address address = new Address("Rua flavao", "Bairro Oliveira", "Aracaju", "Sergipe", "987909123");
+
+        HealthUnit healthUnit = new HealthUnit(null, UnitTypeEnum.HOSPITAL,
+                "Hospital Inacio", "21.222.333/0001-24", address);
+
+        this.healthUnitRepository.create(healthUnit);
+        assertTrue(healthUnit.equals(this.healthUnitRepository.findById(healthUnit.getId())));
+        deleteHealthUnitUseCase.handle(healthUnit.getCNPJ());
+        assertNull(this.healthUnitRepository.findById(healthUnit.getId()));
+
+        try {
+            deleteHealthUnitUseCase.handle(healthUnit.getCNPJ());
+            fail();
+        }catch (Error ignored) {
+        }
+    }
 }
