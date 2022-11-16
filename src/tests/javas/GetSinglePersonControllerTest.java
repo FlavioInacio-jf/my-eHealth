770a67c881a68e9a714665f87f718002681ede47
https://gitlab.com/FlavioInacio-jf <jflavioinacio22@gmail.com>
2022-11-15 21:17:18 -0300

    refactor: Add getSinglePerson tests

--- src/tests/javas/GetSinglePersonControllerTest.java
+++ src/tests/javas/GetSinglePersonControllerTest.java
@@ -1,10 +1,49 @@
 package tests.javas;
 
+import javas.modules.app.models.Address;
+import javas.modules.person.enums.BloodTypeEnum;
+import javas.modules.person.enums.SexEnum;
+import javas.modules.person.models.Person;
+import javas.modules.person.repositories.IPersonRepository;
+import javas.modules.person.repositories.implementations.PersonRepository;
+import javas.modules.person.useCases.getSinglePerson.GetSinglePersonController;
+import javas.modules.person.useCases.getSinglePerson.GetSinglePersonUseCase;
+import javas.modules.vaccine.repositories.IVaccineRepository;
+import javas.modules.vaccine.repositories.impementations.VaccineRepository;
 import org.junit.Test;
 
+import static org.junit.Assert.assertTrue;
+
 public class GetSinglePersonControllerTest {
-    public GetSinglePersonControllerTest() {}
+    IPersonRepository personRepository;
+    IVaccineRepository vaccineRepository;
+
+    GetSinglePersonUseCase getSinglePersonUseCase;
+
+    public GetSinglePersonControllerTest() {
+        this.personRepository = new PersonRepository();
+        this.vaccineRepository = new VaccineRepository();
+        this.getSinglePersonUseCase = new GetSinglePersonUseCase(this.personRepository, this.vaccineRepository);
+    }
 
     @Test
-    public void testExecute() {}
+    public void testExecute() {
+        GetSinglePersonController getSinglePersonController = new GetSinglePersonController(this.getSinglePersonUseCase);
+        Address address = new Address("Abbey Road","Farolandia" , "Paris",
+                "Florida", "586939291");
+        Person person = new  Person(null, "Inácio",
+                "Santos", "896.7423.423-98", BloodTypeEnum.A_LESS, SexEnum.MALE, "11/09/2001", address);
+
+        try {
+            this.personRepository.create(person);
+            assertTrue(getSinglePersonController.execute(person.getCPF()).equals(person));
+
+        }catch (Error | NullPointerException ignored) {
+        }
+
+        try {
+            this.personRepository.delete(person.getId());
+        }catch (Error ignored) {
+        }
+    }
 }
--- src/tests/javas/GetSinglePersonUseCaseTest.java
+++ src/tests/javas/GetSinglePersonUseCaseTest.java
@@ -1,12 +1,47 @@
 package tests.javas;
 
+import javas.modules.app.models.Address;
+import javas.modules.person.enums.BloodTypeEnum;
+import javas.modules.person.enums.SexEnum;
+import javas.modules.person.models.Person;
+import javas.modules.person.repositories.IPersonRepository;
+import javas.modules.person.repositories.implementations.PersonRepository;
+import javas.modules.person.useCases.getSinglePerson.GetSinglePersonUseCase;
+import javas.modules.vaccine.repositories.IVaccineRepository;
+import javas.modules.vaccine.repositories.impementations.VaccineRepository;
 import org.junit.Test;
 
-public class GetSinglePersonUseCaseTest {
+import static org.junit.Assert.assertTrue;
 
-    public GetSinglePersonUseCaseTest() {}
+public class GetSinglePersonUseCaseTest {
+    IPersonRepository personRepository;
+    IVaccineRepository vaccineRepository;
+    public GetSinglePersonUseCaseTest() {
+        this.personRepository = new PersonRepository();
+        this.vaccineRepository = new VaccineRepository();
+    }
 
 
     @Test
-    public void testHandle() {}
+    public void testHandle() {
+        GetSinglePersonUseCase getSinglePersonUseCase = new GetSinglePersonUseCase(this.personRepository, this.vaccineRepository);
+
+        Address address = new Address("Abbey Road","Farolandia" , "Paris",
+                "Florida", "586939291");
+        Person person = new  Person(null, "Inácio",
+                "Santos", "356.555.423-98", BloodTypeEnum.A_LESS, SexEnum.MALE, "11/09/2001", address);
+
+        try {
+            this.personRepository.create(person);
+            assertTrue(getSinglePersonUseCase.handle(person.getCPF()).equals(person));
+
+        }catch (Error | NullPointerException ignored) {
+        }
+
+        try {
+            this.personRepository.delete(person.getId());
+        }catch (Error ignored) {
+        }
+
+    }
 }
